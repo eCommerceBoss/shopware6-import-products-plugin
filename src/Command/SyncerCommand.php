e9eae61a8a2bd98c17e0da94c84c39c7074b055d
Ivan <Ivangenadich@inbox.ru>
2021-04-08 08:14:31 -0700

    add command cli to syncer

--- src/Command/SyncerCommand.php
+++ src/Command/SyncerCommand.php
@@ -5,11 +5,25 @@ namespace Sas\SyncerModule\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Sas\SyncerModule\Service\WritingProductData;
+use Shopware\Core\Framework\Context;
+use Doctrine\DBAL\Connection;
 
 class SyncerCommand extends Command
 {
     // Command name
     protected static $defaultName = 'syncer-commands:start';
+
+    private $writingData;
+
+    private $connection;
+
+    public function __construct(WritingProductData $writingData, Connection $connection)
+    {
+        parent::__construct();
+        $this->writingData = $writingData;
+        $this->connection = $connection;
+    }
 
     // Provides a description, printed out in bin/console
     protected function configure(): void
@@ -20,6 +34,9 @@ class SyncerCommand extends Command
     // Actual code executed in the command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+
+        $context = Context::createDefaultContext();
+        $this->writingData->writeData($context, $this->connection);
         $output->writeln('It works!');
 
         // Exit code 0 for success
