fbfef478cadec88e22659ecc06a4f6f39a485143
Ivan <Ivangenadich@inbox.ru>
2021-04-08 17:06:35 -0700

    version upgrade command sync

--- src/Command/SyncerCommand.php
+++ src/Command/SyncerCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Sas\SyncerModule\Service\WritingProductData;
 use Shopware\Core\Framework\Context;
 use Doctrine\DBAL\Connection;
+use SimpleXMLElement;
 
 class SyncerCommand extends Command
 {
@@ -36,8 +37,67 @@ class SyncerCommand extends Command
     {
 
         $context = Context::createDefaultContext();
-        // $this->writingData->writeData($context, $this->connection);
-        $output->writeln('It works!');
+        $url = "http://109.237.219.217/api/articlefeed/";
+        $token = "wHVs3S7yMKtmvPHSVWj99naCnqdX4WaTVwCVT5rp";
+        
+        $ch = curl_init();
+        $headers = array(
+            'token: '.$token
+        );
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Timeout in seconds
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+        $xmlString = curl_exec($ch);
+        
+        $productes = new SimpleXMLElement($xmlString);
+        $data = [];
+        foreach ($productes->article as $article) {
+            $id = (int)$article->articleid;
+            $product = [];
+            $product['extern_id'] = $id;
+            $product['name'] = (string)$article->articledescription;
+            $product['product_number'] = (string)$article->articlecode;
+            $product['stock'] = (int)$article->articlecurrentstock;
+            $price = (string)$article->articlepurchaseprice;
+            $price = str_replace(',', '.', $price);
+            $product['price'] = number_format((float)$price, 2, '.', '');
+            $product['category'] = [];
+            foreach ($article->categories->categorie as $categorie) {
+                $category = [];
+                $category['extern_id'] = (int)$categorie['extern_id'];
+                $category['parent_extern_id'] = (int)$categorie['parent_extern_id'];
+                $category['name'] = (string)$categorie;
+                $product['category'][] = $category;
+            }
+            $product['property'] = [];
+            foreach ($article->attributes->attributegroup as $attributegroup) {
+                foreach ($attributegroup->attribute as $attribute) {
+                    $property_group_option = [];
+                    $property_group_option['extern_id'] = (int)$attribute['extern_id'];
+                    $property_group_option['name'] = (string)$attribute['name'];
+                    $property_group_option['value'] = (string)$attribute->values->value;
+                    $property_group_option['type'] = (string)$attribute['type'];
+                    $product['property'][] = $property_group_option; 
+                }
+            }
+            
+            $data[] = $product;
+        }
+
+            $output->writeln('product importing started...');
+        foreach ($data as $key => $value) {
+            $context = Context::createDefaultContext();
+            $this->writingData->writeData($value, $context, $this->connection);
+            $output->writeln($value['name']);
+            $output->writeln('');
+        }     
+            $output->writeln('product importing end');
 
         // Exit code 0 for success
         return 0;
